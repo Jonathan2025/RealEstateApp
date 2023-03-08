7e78d629ad845077b5c5dcdefdec9b76dc97835c
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-03-07 21:08:54 -0500

    created the index route using the new mongoose format

--- controllers/houses.js
+++ controllers/houses.js
@@ -7,14 +7,31 @@ const router = express.Router()
 
 
 //access the houses.js file 
-const Product = require('../models/houses.js')
+const Houses = require('../models/houses.js')
 
 
 
 // ROUTES - INDUCES
 
-router.get("/", (req, res) => {
-    res.send('The route really works!')
+
+// with the new version of mongoose, we had to modify the routes and add some new concepts
+// Promise - an object that represents the eventual completeion of an asynchronous operation and its resulting value
+// when you dont want to block the main thread as you wait for a response, so you create a promise to represent the eventual completion of the operation
+// Async - used to declare the route handler function as asynchromous --> since we are using the "await" keyword
+// to wait for the result of Houses.find
+// Asynchronous - program moves on to next task and then comes back to the previouse task once its finished
+// await - pause the execution of the function until a promise is resolved
+
+
+// Index Route 
+router.get("/", async(req,res)=> {
+    try{
+        const allHouses = await Houses.find({})
+        res.render('index.ejs', {houses:allHouses})
+    } catch(error){
+        console.log(error)
+        res.send(error)
+    }
 })
 
 
