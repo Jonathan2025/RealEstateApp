0aaf83981b97f06decf79bd90bbbfebb067de882
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-03-07 21:22:43 -0500

    created a seed route with some dummy data

--- controllers/houses.js
+++ controllers/houses.js
@@ -34,7 +34,44 @@ router.get("/", async(req,res)=> {
     }
 })
 
-
+// SEED DATA
+// Created some dummy data to get the user started. 
+router.get('/seed', async (req, res) => {
+    const newHouse =
+      [
+        {
+            name: 'Count Dooku',
+            address: '1037 Southern Artery',
+            description: 'Availiable Apartment in Faxon Commons',
+            img: 'https://photos.zillowstatic.com/fp/b66c988c1d4909a594f7e61c6657cf94-cc_ft_1536.webp',
+            price: 280000,
+            bedrooms:2,
+            bathrooms: 1,
+            sqft: 848,
+            homeType: 'Townhouse',
+            heating: '',
+            cooling: 'Central Air',
+            parking: 'Spaceous parking lot',
+            yearBuilt: 2008,
+            appliancesIncluded: 'microwave, refridgerator',
+            basement: false,
+            garage: true,
+            kitchen: true,
+            livingRoom: true,
+            diningRoom: false,
+            attic: false,
+            amenities: 'Shared indoor gym, pool and laundry room. Also, there is cafe on the first floor',
+            notes: 'I think this townhouse is great because it is walking distance from walmart'
+        }
+      ]
+  
+    try {
+      const seedItems = await Houses.create(newHouse)
+      res.send(seedItems)
+    } catch (err) {
+      res.send(err.message)
+    }
+  })
 
 
 
