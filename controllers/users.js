89a8311f94ef2809c583f431c8a2933909a76618
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-03-12 00:30:36 -0500

    adding user authentication - created the signin GET and POST routes

--- controllers/users.js
+++ controllers/users.js
@@ -29,11 +29,48 @@ router.post('/register', async (req, res) => {
   
       const newUser = await User.create(req.body)
       req.session.currentUser = newUser
-      res.redirect('/houses')
+      res.redirect('/users/signin')
     } catch (error) {
       console.log(error)
       res.render(error)
     }
   })
+
+// SIGNIN PAGE
+// Signin - get route
+router.get('/signin', (req, res) => {
+	res.render('users/signin.ejs')
+})
+
+
+// Signin - POST route 
+router.post('/signin', async (req, res) => {
+    try {
+      // we need to get the user with that username
+      const foundUser = await User.findOne({ username: req.body.username })
+  
+      if (foundUser) {
+        // if the user does exist we need to compare their passwords using bcrypt's compare function
+        const validLogin = await bcrypt.compare(req.body.password, foundUser.password)
+  
+        //validLogin will be true if the passwords match
+        if (validLogin) {
+          // saving the current user session as the found user
+          req.session.currentUser = foundUser
+
+          res.redirect('/houses')
+        } else {
+          res.send('Invalid username or password')
+        }
+      } else {
+        // if the user does not exist, we need to let user know
+        res.send('The username or password entered does not exist')
+      }
+    } catch (error) {
+      console.log(error)
+      res.render(error)
+    }
+  })
+
 
 module.exports = router
